bcff3e7a6b63aa8f28766aa66dc56ca19480e215
Orphoros <orp@orphoros.dev>
2022-07-31 18:26:48 +0200

    feat: add util function for safely shutting down the system

---
+++ src/util/shutdown.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import { DiscordClient } from '../typings/client';
+import { appLog } from '../util/dbg';
+
+export default function shutdown(client: DiscordClient, exitCode?: number): void {
+  appLog('Shutting down...');
+  client.destroy();
+  mongoose.connection.close().then(() => {
+    appLog('Disconnected from MongoDB!');
+  }).catch((e) => {
+    appLog('Could not disconnect from MongoDB: %O', e);
+  });
+  process.exit(exitCode ?? 0);
+}
