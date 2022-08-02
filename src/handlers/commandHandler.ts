963999d93cafcba0ddfa4c02e6e6c5bb7cf0d2d1
Orphoros <orp@orphoros.dev>
2022-08-02 12:00:54 +0200

    feat: add command file handler

---
+++ src/handlers/commandHandler.ts
@@ -0,0 +1,67 @@
+import { ApplicationCommandDataResolvable, Collection } from 'discord.js';
+import { bootCommandLog, errorLog } from '../util/dbg';
+import { DiscordClient } from '../typings/client';
+import getFiles from '../util/getFiles';
+
+export default async (client: DiscordClient) => {
+  if (!client.user || !client.application) {
+    return;
+  }
+
+  if (process.env.RESET_COMMANDS === 'true') {
+    bootCommandLog('Clearing registered commands...');
+
+    if (process.env.ENV === 'production') {
+      await client.application.commands
+        .set([])
+        .then(() => bootCommandLog('Cleared globally registered commands!'))
+        .catch((e) => errorLog('Could not clear globally registered commands: %O', e));
+    }
+
+    const devGuild = await client.guilds.fetch(process.env.DEV_GUILD_ID ?? '');
+    if (devGuild && devGuild.commands && process.env.ENV === 'development') {
+      await devGuild.commands.set([])
+        .then(() => bootCommandLog('Cleared development registered commands!'))
+        .catch((e) => errorLog('Could not clear registered commands for the guild: %O', e));
+    }
+  }
+
+  const commands:ApplicationCommandDataResolvable[] = [];
+
+  const suffix = '.cmd.ts';
+  const commandFiles = getFiles('./src/commands', suffix);
+
+  client.commands = new Collection();
+  for (const command of commandFiles) {
+    try {
+      const split = command.replace(/\\/g, '/').split('/');
+      const commandName = split[split.length - 1].replace(suffix, '');
+      bootCommandLog('Loading command %o', commandName);
+      // eslint-disable-next-line max-len
+      // eslint-disable-next-line import/no-dynamic-require, global-require, @typescript-eslint/no-var-requires
+      let commandFile = require(command);
+      if (commandFile.default) commandFile = commandFile.default;
+
+      client.commands.set(commandFile.data.name, commandFile);
+      commands.push(commandFile.data.toJSON());
+    } catch (e) {
+      errorLog(`Error loading command file '${command}': %O`, e);
+    }
+  }
+
+  if (process.env.RESET_COMMANDS === 'true') {
+    if (process.env.ENV === 'production') {
+      client.application.commands
+        .set(commands)
+        .then(() => bootCommandLog('Registered commands for production'))
+        .catch((e) => errorLog('Could not register commands for production: %O', e));
+    } else if (process.env.DEV_GUILD_ID && process.env.ENV === 'development') {
+      client.application.commands
+        .set(commands, process.env.DEV_GUILD_ID)
+        .then(() => bootCommandLog('Registered commands for development'))
+        .catch((e) => errorLog('Could not register commands for development: %O', e));
+    } else {
+      errorLog('Could not register commands: No environment set');
+    }
+  }
+};
