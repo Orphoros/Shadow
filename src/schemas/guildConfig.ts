984621d2722730335c0171e6542ef225c1173ec6
Orphoros <orp@orphoros.dev>
2022-08-02 11:58:59 +0200

    feat(mongo): define bot per guild config schema

---
+++ src/schemas/guildConfig.ts
@@ -0,0 +1,54 @@
+import {
+  model, Schema, Model, Document,
+} from 'mongoose';
+
+export interface IBotGuildConfig extends Document {
+    guild_id?: string;
+    welcome_channel_id?: string;
+    announcement_channel_id?: string;
+    main_channel_id?: string;
+    admin_users?: string[];
+    admin_roles?: string[];
+    auto_vc_channel_name?: string;
+  }
+
+const schema: Schema = new Schema({
+  guild_id: {
+    type: String,
+    required: true,
+    nullable: false,
+    unique: true,
+  },
+  welcome_channel_id: {
+    type: String,
+    required: false,
+    nullable: true,
+  },
+  announcement_channel_id: {
+    type: String,
+    required: false,
+    nullable: true,
+  },
+  main_channel_id: {
+    type: String,
+    required: false,
+    nullable: true,
+  },
+  admin_users: {
+    type: [String],
+    required: false,
+    nullable: true,
+  },
+  admin_roles: {
+    type: [String],
+    required: false,
+    nullable: true,
+  },
+  auto_vc_channel_name: {
+    type: String,
+    required: false,
+    nullable: true,
+  },
+});
+
+export const BotGuildConfig: Model<IBotGuildConfig> = model<IBotGuildConfig>('BOT_CONFIG_GUILD', schema, 'BOT_CONFIG_GUILD');
