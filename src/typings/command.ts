3a1573fdbccd00cee3f3012bef26b094e73c810f
Orphoros <orp@orphoros.dev>
2022-08-02 11:56:36 +0200

    feat: add DiscordClient to CommandSnippet typing

--- src/typings/command.ts
+++ src/typings/command.ts
@@ -1,6 +1,7 @@
 import { ApplicationCommand, CacheType, CommandInteraction } from 'discord.js';
+import { DiscordClient } from './client';
 
 export interface CommandSnippet {
    data: ApplicationCommand;
-   execute: (i: CommandInteraction<CacheType>) => Promise<void>;
+   execute: (i: CommandInteraction<CacheType>, c: DiscordClient) => Promise<void>;
 }
