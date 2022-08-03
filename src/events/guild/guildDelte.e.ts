a64afbe637f1700fa83f5eb1d2974dd0442e43d0
Orphoros <orp@orphoros.dev>
2022-08-03 08:31:06 +0200

    feat: add bot guild leve event detector

---
+++ src/events/guild/guildDelte.e.ts
@@ -0,0 +1,31 @@
+import { DiscordClient } from '../../typings/client';
+import { errorLog, eventLog } from '../../util/dbg';
+import { BotGuildConfig } from '../../schemas/guildConfig';
+import { SelectableRoleOption } from '../../schemas/selectableRoleOption';
+
+export default (client: DiscordClient): void => {
+  client.on('guildDelete', (guild) => {
+    eventLog(`Guild left: ${guild.name} (${guild.id})`);
+    const query = {
+      guild_id: guild?.id,
+    };
+
+    BotGuildConfig.findOneAndDelete(query)
+      .then(() => {
+        eventLog(`Removed guild config for "${guild.name}" (${guild.id})`);
+      })
+      .catch((e) => {
+        eventLog(`Could not remove guild config for "${guild.name}" (${guild.id})`);
+        errorLog('DB error: %O', e);
+      });
+
+    SelectableRoleOption.deleteMany({ guild_id: guild?.id })
+      .then(() => {
+        eventLog(`Removed guild selectable role options for "${guild.name}" (${guild.id})`);
+      })
+      .catch((e) => {
+        eventLog(`Could not remove guild selectable role options for "${guild.name}" (${guild.id})`);
+        errorLog('DB error: %O', e);
+      });
+  });
+};
