d3fd09179bc2c2a520ea96a2ab69aeeb19d1b26f
Orphoros <orp@orphoros.dev>
2022-08-05 13:19:41 +0200

    feat: add main app logic

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import { Client, Intents } from 'discord.js';
+import * as dotenv from 'dotenv';
+import { DiscordClient } from './typings/client';
+import eventHandler from './handlers/eventHandler';
+import { bootLog } from './util/dbg';
+
+dotenv.config();
+
+const init = async () => {
+  bootLog('Bot is starting...');
+
+  const client = new Client({
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.GUILD_VOICE_STATES,
+      Intents.FLAGS.GUILD_PRESENCES,
+      Intents.FLAGS.GUILD_MEMBERS,
+    ],
+  }) as DiscordClient;
+
+  eventHandler(client);
+
+  client.login(process.env.BOT_TOKEN);
+};
+
+init();
