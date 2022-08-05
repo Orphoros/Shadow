fa0abf173aa12ea03ad20361fad3d0d4a45194d7
Orphoros <orp@orphoros.dev>
2022-08-05 15:50:41 +0200

    feat(mongo): add selectable color schema

---
+++ src/schemas/selectableColorRoleOption.ts
@@ -0,0 +1,33 @@
+import {
+  model, Schema, Model, Document,
+} from 'mongoose';
+
+export interface ISelectableColorRoleOption extends Document {
+  guild_id: string;
+  color_role_id: string;
+  color_description?: string;
+  color_emoji?: string;
+}
+
+const schema: Schema = new Schema({
+  guild_id: {
+    type: String,
+    required: true,
+  },
+  color_role_id: {
+    type: String,
+    required: true,
+  },
+  color_description: {
+    type: String,
+    required: false,
+  },
+  color_emoji: {
+    type: String,
+    required: false,
+  },
+});
+
+schema.index({ guild_id: 1, color_role_id: 1 }, { unique: true });
+
+export const SelectableColorRoleOption: Model<ISelectableColorRoleOption> = model<ISelectableColorRoleOption>('SELECTABLE_ROLES_COLOR', schema, 'SELECTABLE_ROLES_COLOR');
