ca9a487c6f878df04c80631ade2cb05690828bfc
Orphoros <orp@orphoros.dev>
2022-08-25 19:41:57 +0200

    feat: update schema export index

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,3 +2,8 @@ export * from './botStatus';
 export * from './guildConfig';
 export * from './selectableColorRoleOption';
 export * from './selectableRoleOption';
+export * from './selectableAgeRoleOption';
+export * from './selectablePingRoleOption';
+export * from './selectableRegionRoleOption';
+export * from './selectablePronounRoleOption';
+export * from './selectableDMRoleOption';
